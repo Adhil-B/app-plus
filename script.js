08b604b90936bf1172534dd08abce647e6b1a1da
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-09-05 01:28:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Saveetha Food App+
+// @namespace    http://tampermonkey.net/
+// @version      2024-09-04
+// @description  try to take over the world!
+// @author       Adhil
+// @match        https://life.saveetha.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=saveetha.com
+// @grant        none
+
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+})();
